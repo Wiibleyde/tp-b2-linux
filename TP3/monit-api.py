8b859b14abdbc41438bfed4e8405becdc482a3dc
Wiibleyde <nathan@bonnell.fr>
2024-01-18 10:07:48 +0100

    Add error handling for 404 response
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP3/monit-api.py
+++ TP3/monit-api.py
@@ -24,6 +24,10 @@ def requestLast():
 def requestAvg(hours):
     return Calls.requestAvg(hours)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"error": "404"}, 404
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
