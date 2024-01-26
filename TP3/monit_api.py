03ddc9ce92ddf19a2349de77638553e5fd388429
Wiibleyde <nathan@bonnell.fr>
2024-01-26 10:06:34 +0100

    Add Monit API script with Flask and Swagger
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- TP3/monit_api.py
+++ TP3/monit_api.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""Monit API
+
+This script allows to run the Monit API.
+
+This script requires that `flask` and `flasgger` be installed within the Python
+environment you are running this script in.    
+"""
+
 from flask import Flask
 from flasgger import Swagger
 from monit import Calls
@@ -20,7 +29,7 @@ def status():
 
 
 @app.route("/run/check", methods=["GET"])
-def requestCheck():
+def request_check():
     """
     The check request endpoint
     ---
@@ -28,12 +37,12 @@ def requestCheck():
       200:
         description: Check request
     """
-    Calls.requestCheck()
+    Calls.request_check()
     return {"status": "ok"}
 
 
 @app.route("/reports", methods=["GET"])
-def requestReports():
+def request_reports():
     """
     The reports endpoint
     ---
@@ -41,14 +50,14 @@ def requestReports():
       200:
         description: Reports
     """
-    reports = Calls.requestReports()
+    reports = Calls.request_reports()
     if reports is None:
         return {"error": "No report found"}, 404
     return reports
 
 
 @app.route("/reports/latest", methods=["GET"])
-def requestLast():
+def request_last():
     """
     The latest report endpoint
     ---
@@ -56,19 +65,19 @@ def requestLast():
       200:
         description: Latest report
     """
-    last = Calls.requestLast()
+    last = Calls.request_last()
     if last is None:
         return {"error": "No report found"}, 404
     return last
 
 
-@app.route("/reports/<string:id>", methods=["GET"])
-def requestReport(id):
+@app.route("/reports/<string:id_wanted>", methods=["GET"])
+def request_report(id_wanted):
     """
     The report endpoint
     ---
     parameters:
-      - name: id
+      - name: id_wanted
         in: path
         type: string
         required: true
@@ -77,14 +86,14 @@ def requestReport(id):
       200:
         description: Report
     """
-    report = Calls.requestReport(id)
+    report = Calls.request_report(id_wanted)
     if report is None:
         return {"error": "No report found"}, 404
     return report
 
 
 @app.route("/reports/avg/<int:hours>", methods=["GET"])
-def requestAvg(hours):
+def request_avg(hours):
     """
     The average report endpoint
     ---
@@ -98,7 +107,7 @@ def requestAvg(hours):
       200:
         description: Average report
     """
-    avg = Calls.requestAvg(hours)
+    avg = Calls.request_avg(hours)
     if avg is None:
         return {"error": "No report found"}, 404
     return avg
@@ -106,17 +115,41 @@ def requestAvg(hours):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return {"error": "404"}, 404
+    """Error 404
+
+    Args:
+        e (string): Error message
+
+    Returns:
+        json: Error message
+    """
+    return {"error": "404","message":e}, 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return {"error": "500"}, 500
+    """Error 500
+
+    Args:
+        e (string): Error message
+
+    Returns:
+        json: Error message
+    """
+    return {"error": "500","message":e}, 500
 
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    return {"error": "405"}, 405
+    """Error 405
+
+    Args:
+        e (string): Error message
+
+    Returns:
+        json: Error message
+    """
+    return {"error": "405","message":e}, 405
 
 
 if __name__ == "__main__":
